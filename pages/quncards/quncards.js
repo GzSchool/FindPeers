04e05aceca85ff5c3b43297da8f0089846bf7e35
dkh <252490171@qq.com>
2018-08-01 13:58:14 +0800

    dkh qun fix mine

--- pages/quncards/quncards.js
+++ pages/quncards/quncards.js
@@ -1,39 +1,22 @@
    // pages/quncards/quncards.js
 var util = require('../../utils/util.js');
-var app=getApp() 
+var app = getApp()
 Page({
   data: {
-  qunname:"格致文化",
-  nonum:0,
-  num:1,
-  server:"",
-  time:"2018/7/16",
-  list: [],
-  hidden: true,
-  scrollTop: 0,
-  scrollHeight: 0
+    list: []
   },
-  onLoad:function(a){
+  onLoad: function (a) {
+    console.log('load')
     var that = this;
-    that.data.server=app.globalData.server
-    var server = that.data.server
-    wx.getSystemInfo({
-      success: function (res) {
-        console.info(res.windowHeight);
-        that.setData({
-          scrollHeight: res.windowHeight
-        });
-      }
-    });
-    that.setData({
-      list:[]
-    })
-    var openid=app.globalData.openid;
+    // that.setData({
+    //   list: []
+    // })
+    var openid = app.globalData.openid;
     var groupid = app.globalData.groupid;
-    var list=that.data.list;
-    util.getUserGroupById(openid).then(function(res){
-      var length = res.data.data.length;
-      for (var i = 0; i < length; i++) {
+    var list = that.data.list;
+    util.getUserGroupById(openid).then(function (res) {
+      var len = res.data.data.length;
+      for (var i = 0; i < len; i++) {
         // 处理时间截取字母T之前的年月日
         res.data.data[i].ctTime = res.data.data[i].ctTime.split('T')[0];
         list.push(res.data.data[i]);
@@ -41,21 +24,16 @@ Page({
       that.setData({
         list: list
       });
-      that.setData({
-        hidden: true
-      });
     })
   },
-  search:function(a){
-   console.log(a)
-   var openid = a.currentTarget.dataset.id;
-   var groupid=a.currentTarget.dataset.key;
-   console.log(groupid)
+  search: function (a) {
+    var openid = a.currentTarget.dataset.id;
+    var groupid = a.currentTarget.dataset.key;
     wx.navigateTo({
-      url: '/pages/teampeers/teampeers?openid='+openid+'&groupid='+groupid,
+      url: '/pages/teampeers/teampeers?openid=' + openid + '&groupid=' + groupid,
     })
   },
-  onShow:function(){
+  onShow: function () {
     this.onLoad();
   }
 })
