9b352ec78548688309afe2e45ed55354f629d0b6
dkh <252490171@qq.com>
2018-08-15 11:50:43 +0800

    otherpeers.wxss pdt peerscards.wxss pdt fix.js fix.wxml

--- utils/validate.js
+++ utils/validate.js
@@ -131,9 +131,6 @@ export function cardid(code) {
  * 判断手机号码是否正确
  */
 export function isvalidatemobile(phone) {
-  if (phone == "" || phone == null) {
-    return true;
-  }
   var reg = /^(((13[0-9]{1})|(14[0-9]{1})|(17[0-9]{1})|(15[0-3]{1})|(15[5-9]{1})|(18[0-9]{1}))+\d{8})$/
   return reg.test(phone)
 }
